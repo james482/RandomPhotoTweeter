cfc4d12c1b1d8e99dfd2146fff1662a0706c219a
james482 <jms_482@yahoo.co.uk>
2016-01-29 11:02:37 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,53 +1,117 @@
 import os
 import sys
 import logging
-import tweepy
-import config
-import weekday
-import q
-
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
-
-# is there a file path given
-if len(sys.argv) > 2:
-    dir = str(sys.argv[2])
-    print(sys.argv[2])
-    while not os.path.exists(dir):
-        dir = input('Not a valid file path, please try again: ')
-else:
-    dir = input("Enter your picture folder path: ")
-    while not os.path.exists(dir):
-        dir = input('Not a valid file path, please try again: ')
-
-settings = config.Config(dir)
-
-mode = sys.argv[1]
-
-if mode == 'weekday':
-    post = weekday.post(settings, dir)
-elif mode == 'queue':
-    post = q.post(settings, dir)
-
-auth = tweepy.OAuthHandler(settings.get_consumer_key(), settings.get_consumer_secret())
-auth.secure = True
-auth.set_access_token(settings.get_access_token(), settings.get_access_token_secret())
-
-tweet = tweepy.API(auth)
+import json
 
 
-pic = post[0]
-status = post[1]
-try:
-    logging.disable(logging.INFO)
-    #tweet.update_with_media(pic, status=status)
-    os.startfile(pic)
-    logging.disable(logging.NOTSET)
-except:
-    print('Authentication failed, please check your keys in the congif file.')
-    os.system("Pause")
-    sys.exit(1)
+class Config:
+    def __create(self, dir):
+        key = {'consumerKey': input('Enter your Consumer key: '),
+               'consumerSecret': input('Enter your Consumer Secret: '),
+               'accessToken': input('Enter your Access Token: '),
+               'accessTokenSecret': input('Enter your Access Token Secret: ')
+               }
 
-settings.set_old_pic(pic)
-settings.save()
+        monday = ['I love Mondays', 'I cant believe its already Monday', 'It\'s Monday']
+        tuesday = ['I love Tuesdays', 'I cant believe its already Tuesday', 'It\'s Tuesday']
+        wednesday = ['I love Wednesdays', 'I cant believe its already Wednesday', 'It\'s Wednesday']
+        thursday = ['I love Thursdays', 'I cant believe its already Thursday', 'It\'s Thursday']
+        friday = ['I love Fridays', 'I cant believe its already Friday', 'It\'s Friday']
+        saturday = ['I love Saturdays', 'I cant believe its already Saturday', 'It\'s Saturday']
+        sunday = ['I love Sundays', 'I cant believe its already Sunday', 'It\'s Sunday']
 
-logging.debug('Done, successfully posted: ' + pic + ', with the status: ' + status)
+        queue = ['last', 'fourth', 'third', 'second', 'first']
+
+        status = {'Monday': monday,
+                  'Tuesday': tuesday,
+                  'Wednesday': wednesday,
+                  'Thursday': thursday,
+                  'Friday': friday,
+                  'Saturday': saturday,
+                  'Sunday': sunday}
+
+        oldPic = ''
+
+        fileType = 'jpeg', 'jpg', 'png', 'webp', 'gif'
+
+        maxSize = 3
+
+        settings = {'key': key,
+                    'status': status,
+                    'queue': queue,
+                    'oldPic': oldPic,
+                    'fileType': fileType,
+                    'maxSize': maxSize}
+
+        f = open(os.path.join(dir, 'config.json'), 'w')
+        json.dump(settings, f, indent=4)
+        logging.debug('Creating: ' + os.path.join(dir, 'config.json'))
+        f.close()
+        return settings
+
+    def __load(self, dir):
+        try:
+            f = open(os.path.join(dir, 'config.json'), 'r')
+            logging.debug('Opening: ' + os.path.join(dir, 'config.json'))
+
+            try:
+                settings = json.load(f)
+            except:
+                input('Config file corrupted.')
+                f.close()
+                sys.exit(1)
+            f.close()
+        except IOError:
+            fileMade = False
+            print('No config file in that location, creating one now..')
+            while not fileMade:
+                try:
+                    settings = self.__create(dir)
+                    fileMade = True
+
+                    print('Config file created in : ' + dir)
+                    print('Exit now and open the config.json file if you wish to customize your statuses.')
+                    input('Otherwise continue with the defaults.')
+                except:
+                    dir = input('Not a valid file path, please try again: ')
+        return settings
+
+    def save(self):
+        f = open(os.path.join(self.dir, 'config.json'), 'w')
+        json.dump(self.settings, f, indent=3)
+        f.close()
+
+    def get_consumer_key(self):
+        return self.settings['key']['consumerKey']
+
+    def get_consumer_secret(self):
+        return self.settings['key']['consumerSecret']
+
+    def get_access_token(self):
+        return self.settings['key']['accessToken']
+
+    def get_access_token_secret(self):
+        return self.settings['key']['accessToken']
+
+    def get_file_types(self):
+        return tuple(self.settings['fileType'])
+
+    def get_max_size(self):
+        return self.settings['maxSize']
+
+    def get_old_pic(self):
+        return self.settings['oldPic']
+
+    def set_old_pic(self, pic):
+        self.settings['oldPic'] = pic
+        return
+
+    def get_status_day(self, day):
+        return self.settings['status'][day]
+
+    def get_queue(self):
+        return self.settings['queue']
+
+    def __init__(self, dir):
+        self.settings = self.__load(dir)
+        self.dir = dir
