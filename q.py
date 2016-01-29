6d24d43d39b07ebe63788ad4b5a71b6693d6d7a8
james482 <jms_482@yahoo.co.uk>
2016-01-29 14:00:27 +0000

    Update q.py

--- q.py
+++ q.py
@@ -1,16 +1,11 @@
-import weekday
-import random
-import datetime
+import tweet
 
 
 def post(conf, dir):
-    pic = weekday.post(conf, dir)[0]
-    today = datetime.datetime.now().strftime("%A")
-
     try:
         status = conf.get_queue().pop()
     except IndexError:
-        status = random.choice(conf.get_status_day(today))
+        status = 'What a day!'
 
-    p = [pic, status]
-    return p
+    tweet.tweet(conf, status)
+    return
