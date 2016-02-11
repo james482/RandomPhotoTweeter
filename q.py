79f247bdd8928399996077517b3864f73bc7a13a
james482 <jms_482@yahoo.co.uk>
2016-02-11 07:11:00 +0000

    Update q.py

--- q.py
+++ q.py
@@ -1,11 +1,16 @@
 import tweet
+import os
+import sys
+import logging
 
 
-def post(conf, dir):
+def post(conf):
     try:
         status = conf.get_queue().pop()
     except IndexError:
-        status = 'What a day!'
+        logging.error('The Queue is empty.')
+        os.system("Pause")
+        sys.exit(1)
 
     tweet.tweet(conf, status)
     return
